da93a9fb10fa73e0360e6f32effe4b8570ef4a37
Nazir <gympills@gmail.com>
2023-09-25 00:44:11 +0500

    add create task method

--- src/main/java/com/telegrambot/workqueue/service/TrackerQueueService.java
+++ src/main/java/com/telegrambot/workqueue/service/TrackerQueueService.java
@@ -4,11 +4,32 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TrackerQueueService {
+    private final Map<Long, Map<Integer, String>> channels = new ConcurrentHashMap<>();
+    //add
+    //delete
+    //clear
+    //close
+    //report - составить отчет из всех закрытых тасок
 
+    public Map<Integer, String> addTask(String task, String name, Long chatId) {
 
+        Map<Integer, String> taskMap = chooseChannellMap(chatId);
+        int counter = taskMap.keySet().size() + 1;
+        taskMap.put(counter, task + " @" + name);
+        return taskMap;
+    }
+
+    private Map<Integer, String> chooseChannellMap(Long chatId) {
+        channels.computeIfAbsent(chatId, k -> new HashMap<>());
+        return channels.get(chatId);
+    }
 
 }
--- src/main/java/com/telegrambot/workqueue/telegram/TelegramBot.java
+++ src/main/java/com/telegrambot/workqueue/telegram/TelegramBot.java
@@ -1,40 +1,76 @@
 package com.telegrambot.workqueue.telegram;
 
 import com.telegrambot.workqueue.config.BotConfig;
+import com.telegrambot.workqueue.service.TrackerQueueService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class TelegramBot extends TelegramLongPollingBot {
+    public static final String ADD_TASK = "/add";
+    public static final String SHOW = "/show";
+    public static final String TASK_IS_ADDED_IN_QUEUE = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n Таска добавлена в очередь \n";
+    public static final String END_TITLE = "\"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n";
+    private static final String MK_PC = "https://tenor.com/ru/view/monkey-stress-mad-gif-15327798";
     private final BotConfig botConfig;
-    public final Map<String, Integer> map = new HashMap<>();
-
+    private final TrackerQueueService trackerService;
 
     @Override
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
             // Set variables
-            String message_text = update.getMessage().getText();
-            long chat_id = update.getMessage().getChatId();
+            String msgTxt = update.getMessage().getText();
+            String command = prepareCommand(msgTxt);
+            String task = prepareTask(msgTxt);
+            String name = update.getMessage().getFrom().getFirstName();
+            long chatId = update.getMessage().getChatId();
 
-            SendMessage message = new SendMessage(); // Create a message object object
-            message.setChatId(chat_id);
-            message.setText(message_text);
-            try {
-                execute(message); // Sending our message object to user
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            switch (command) {
+                case ADD_TASK -> {
+                    Map<Integer, String> taskMap = trackerService.addTask(task, name, chatId);
+                    StringBuilder taskList = new StringBuilder();
+                    AtomicInteger count = new AtomicInteger(1);
+                    String ans = buildTaskList(taskMap, taskList, count, MK_PC);
+                    sendMessage(chatId, ans);
+                }
+                case SHOW -> {
+                    log.info("");
+                }
             }
         }
+    }
+
+    private static String buildTaskList(
+            Map<Integer, String> taskMap,
+            StringBuilder taskList,
+            AtomicInteger count,
+            String gif) {
+        taskMap.values().forEach(taskDescription ->
+                taskList.append(count.getAndIncrement())
+                        .append(". ")
+                        .append(taskDescription)
+                        .append("\n")
+        );
+        return TASK_IS_ADDED_IN_QUEUE + taskList + END_TITLE + gif;
+    }
+
+    private String prepareTask(String msgTxt) {
+        return msgTxt.substring(4);
+    }
+
+    private String prepareCommand(String msgTxt) {
+        return msgTxt.toLowerCase().split(" ")[0];
     }
 
     @Override
@@ -47,23 +83,15 @@ public class TelegramBot extends TelegramLongPollingBot {
         return botConfig.getToken();
     }
 
-    private void startCommandReceived(Long chatId, String name) {
-        String answer = "Hi, " + name + ", nice to meet you!" + "\n" +
-                "Enter the currency whose official exchange rate" + "\n" +
-                "you want to know in relation to BYN." + "\n" +
-                "For example: USD";
-        sendMessage(chatId, answer);
-    }
 
-
-    private void sendMessage(Long chatId, String textToSend){
+    private void sendMessage(Long chatId, String textToSend) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
         sendMessage.setText(textToSend);
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
-
+            log.error(e.getMessage());
         }
     }
 
